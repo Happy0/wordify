b0ace4e9c1ea5e1e4a5390ce003593d05e550b78
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-03-01 09:30:35 +0000

    Add helper function to determine direction a word is going in.

--- src/Wordify/Rules/Pos.hs
+++ src/Wordify/Rules/Pos.hs
@@ -20,25 +20,25 @@ module Wordify.Rules.Pos (Pos,
   data Direction = Horizontal | Vertical deriving Eq
 
   posMin :: Int
-  posMin = 1 
+  posMin = 1
 
   posMax :: Int
-  posMax = 15 
+  posMax = 15
 
   posAt :: (Int, Int) -> Maybe Pos
   posAt = flip Map.lookup posMap
 
   -- | The position above the given position, if it exists.
   above :: Pos -> Maybe Pos
-  above (Pos x y _) = posAt (x,y + 1) 
+  above (Pos x y _) = posAt (x,y + 1)
 
   -- | The position below the given position, if it exists.
   below :: Pos -> Maybe Pos
-  below (Pos x y _) = posAt (x, y - 1 ) 
+  below (Pos x y _) = posAt (x, y - 1 )
 
   -- | The position to the left of the given position, if it exists.
   left :: Pos -> Maybe Pos
-  left (Pos x y _) = posAt (x - 1, y) 
+  left (Pos x y _) = posAt (x - 1, y)
 
   -- | The position to the right of the given position, if it exists.
   right :: Pos -> Maybe Pos
@@ -48,6 +48,12 @@ module Wordify.Rules.Pos (Pos,
   starPos :: Pos
   starPos = Pos 8 8 "H8"
 
+  direction :: Pos -> Pos -> Maybe Direction
+  direction startPos endPos
+    | xPos startPos == xPos endPos = Just Horizontal
+    | yPos startPos == yPos endPos = Just Vertical
+    | otherwise = Nothing
+
   {- A map keyed by tuples representing (x,y) co-ordinates, and valued by their
   corresponding Pos types -}
   posMap :: Map.Map (Int, Int) Pos
@@ -55,7 +61,7 @@ module Wordify.Rules.Pos (Pos,
       where
           coordTuples = zipWith makeTuple (sequence [[posMin..posMax], [posMin..posMax]]) $ cycle ['A'..'O']
           makeTuple (x:y:_) gridLetter = Just $ ((y,x) , Pos y x (gridLetter : show x) )
-          makeTuple _ _ = Nothing        
+          makeTuple _ _ = Nothing
 
   xPos :: Pos -> Int
   xPos (Pos x _ _) = x
